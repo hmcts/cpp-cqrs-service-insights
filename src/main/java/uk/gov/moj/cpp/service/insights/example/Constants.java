0c352076685c77f6cdfeea04b877bb6ba2f2e3ce
Satish Kumar <satish.kumar1@hmcts.net>
2024-11-06 22:43:54 +0000

    deleted unused  class

--- src/main/java/uk/gov/moj/cpp/service/insights/example/Constants.java
+++
@@ -1,8 +0,0 @@
-package uk.gov.moj.cpp.service.insights.example;
-
-
-public class Constants {
-    public static final String ROOT_PACKAGE = "uk.gov.moj.cpp";
-    public static final String PROJECT_DIR = "/path/to/your/project/src/main/java"; // Update this path
-    public static final String STATE_VARIABLES_FILE = "/path/to/state_variables.properties"; // Update this path
-}
--- src/main/java/uk/gov/moj/cpp/service/insights/example/Filter.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.moj.cpp.service.insights.example;
-
-
-import java.util.Set;
-
-public interface Filter {
-    Set<String> apply(Set<String> input);
-}
-
